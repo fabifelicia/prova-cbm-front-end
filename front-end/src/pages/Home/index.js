a43a0e337cecf35b2bb08567c26165a173e85ada
fabifelicia <fabianabarretomenezes@gmail.com>
2022-06-02 20:44:21 -0300

    add pages

--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Card, Button, Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Home() {
+  
   return (
-    <div>Home</div>
+   <Container style={{display:'flex', alignItems: 'center', justifyContent: 'center', margin: '100px'}}>
+     <Card style={{width: '18rem', borderRadius: '5rem'}}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <Card.Title>Trabalhe conosco</Card.Title>        
+        <Button style={{margin: '5px', borderRadius: '30px'}} variant="primary">Cadastre-se</Button>
+        <Button style={{margin: '5px', borderRadius: '30px'}} variant="primary">Ver Concorrentes</Button>
+      </Card.Body>
+    </Card>
+    
+   </Container>
+      
+    
   )
 }
--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -2,14 +2,23 @@ import React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Data from './pages/Data'
+import Experience from './pages/Experience'
+import Resume from './pages/Resume'
+import Profile from './pages/Profile'
+import Candidates from './pages/Candidates'
 
 
 export default function Router() {
   return (
       <BrowserRouter>
         <Routes>
-            <Route path="/" exact element={<Home/>} />
-            
+            <Route path="/" exact element={<Home/>} />            
+            <Route path="/data" exact element={<Data/>} />            
+            <Route path="/experience" exact element={<Experience/>} />            
+            <Route path="/resume" exact element={<Resume/>} />            
+            <Route path="/profile" exact element={<Profile/>} />            
+            <Route path="/list" exact element={<Candidates/>} />            
         </Routes>
       </BrowserRouter>
   )
