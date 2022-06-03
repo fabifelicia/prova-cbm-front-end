74785ed7c614f70fe13230f4674c38085af43198
fabifelicia <fabianabarretomenezes@gmail.com>
2022-06-02 21:16:13 -0300

    home styles

--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { Card, Button, Container } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
 export default function Home() {
   
   return (
-   <Container style={{display:'flex', alignItems: 'center', justifyContent: 'center', margin: '100px'}}>
-     <Card style={{width: '18rem', borderRadius: '5rem'}}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+   <Container style={{position: 'relative', width: '1440px', height: '1024px', backgroundColor:'#f7f7f7'}}>
+     <Card style={{width: '589px', height: '623px', borderRadius: '70px', backgroundColor: '#fff', boxShadow:'0px 24px 32px rgba(0, 0, 0, 0.25'}}>
+      <Card.Img variant="top" src="./assets/logo.png" />
       <Card.Body style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        <Card.Title>Trabalhe conosco</Card.Title>        
-        <Button style={{margin: '5px', borderRadius: '30px'}} variant="primary">Cadastre-se</Button>
-        <Button style={{margin: '5px', borderRadius: '30px'}} variant="primary">Ver Concorrentes</Button>
+        <Card.Title style={{fontSize: '40px', lineHeight: '48.41px', color: '#979595'}}>Trabalhe conosco</Card.Title>        
+        <Button style={{margin: '5px', borderRadius: '63px', width: '408px', height: '90px', backgroundColor: 'linear-gradient(91.76deg, #D42F43 0%, #D3823E 100%), #D9D9D9'}}>Cadastre-se</Button>
+        <Button style={{margin: '5px', borderRadius: '30px'}} variant="secondary">Ver Concorrentes</Button>
       </Card.Body>
     </Card>
     
