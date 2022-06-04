b2fd01043aa3f348443c92b469b758d41b724c70
fabifelicia <fabianabarretomenezes@gmail.com>
2022-06-04 09:21:11 -0300

    add server data

--- front-end/src/pages/Candidates/index.js
+++ front-end/src/pages/Candidates/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import * as S from './styled'
@@ -9,6 +9,21 @@ import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 export default function Candidates() {
 
   const navigate = useNavigate()
+  const [candidates, setCandidates] = useState([])
+
+  useEffect(() => {
+    const url = 'http://localhost:3333/perfil'
+    getCandidates(url)
+  }, [navigate])
+
+  async function getCandidates(url) {
+    const response = await fetch(url)
+    const candidates = await response.json()
+    console.log(candidates)
+
+    setCandidates(candidates)
+  }
+
 
   function handleReturn() {
     navigate('/')
@@ -23,12 +38,14 @@ export default function Candidates() {
       <S.DivCard>
         <S.Title>Candidatos</S.Title>
         <S.List>
-          <S.ListGroupItem>Aurora Jesus
-            <S.Buton onClick={handleProfile}>
-              <FontAwesomeIcon style={{marginRight: '5px'}} icon={solid("magnifying-glass")} color='#fff'/>
-              Ver
-            </S.Buton>
-          </S.ListGroupItem>
+          {candidates.map(candidate => (
+            <S.ListGroupItem key={candidate.id}>{candidate.name}
+              <S.Buton onClick={handleProfile}>
+                <FontAwesomeIcon style={{ marginRight: '5px' }} icon={solid("magnifying-glass")} color='#fff' />
+                Ver
+              </S.Buton>
+            </S.ListGroupItem>
+          ))}
         </S.List>
         <S.ButtonReturn onClick={handleReturn}>Voltar</S.ButtonReturn>
       </S.DivCard>
