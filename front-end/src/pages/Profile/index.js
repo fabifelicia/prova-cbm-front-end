5e27dbd5385a4d8c51af25973025dc7a7ee9840a
fabifelicia <fabianabarretomenezes@gmail.com>
2022-06-07 10:55:45 -0300

    styles profile page

--- front-end/src/pages/Profile/index.js
+++ front-end/src/pages/Profile/index.js
@@ -29,34 +29,51 @@ export default function Profile() {
         return (
           <S.DivCard>
             <S.Title>{candidate.name}</S.Title>
-            <S.Label><strong>Nome:</strong>{candidate.name}</S.Label>
-            <S.Label>CPF:{candidate.cpf}</S.Label>
-            <S.Label> Data de Nascimento:{candidate.data_nascimento}</S.Label>
-            <S.Label>Signo:{candidate.signo}</S.Label>
-            <S.Label>Tipo Sanguíneo:{candidate.tipo_sanguineo}</S.Label>
-            <S.Label>E-mail:{candidate.email}</S.Label>
-            <S.Label>Telefone:{candidate.telefone}</S.Label>
-            <S.Title>Resumo</S.Title>
-            <S.Label>{candidate.resumo}</S.Label>
-            <S.Title>Formação</S.Title>
-            
-                  <S.Label>Instituição: {candidate.formação[0].instituicao}</S.Label>
-                  <S.Label>Curso: {candidate.formação[0].curso}</S.Label>
-                
-           
-            <S.Title>Experiência</S.Title>
-            
-                  <S.Label>Empresa: {candidate.experiencia[0].empresa}</S.Label>
-                  <S.Label>Cargo: {candidate.experiencia[0].cargo}</S.Label>
-                  <S.Label>Tempo: {candidate.experiencia[0].tempo}</S.Label>
-             
-            <S.Title>Competências</S.Title>
-            {candidates.map(candidate => {
-              return (
-                <S.Label>{candidate.competencias}</S.Label>
+            <S.ContentContainer>
+              
+              <S.Label><S.LabelStrong>Nome:</S.LabelStrong>{ candidate.name }</S.Label>
+              <S.Content>
+                <S.Label> <S.LabelStrong>CPF:</S.LabelStrong>{candidate.cpf}</S.Label>
+                <S.Label> < S.LabelStrong>Data de Nascimento:</S.LabelStrong>{candidate.data_nascimento}</S.Label>
+                <S.Label> <S.LabelStrong>Signo:</S.LabelStrong>{candidate.signo}</S.Label>
+              </S.Content>
+              <S.Label><S.LabelStrong>Tipo Sanguíneo:</S.LabelStrong>{candidate.tipo_sanguineo}</S.Label>
+              <S.Content>
+                <S.Label><S.LabelStrong>E-mail:</S.LabelStrong>{candidate.email}</S.Label>
+                <S.Label><S.LabelStrong>Telefone:</S.LabelStrong>{candidate.telefone}</S.Label>
+              </S.Content>
+              
+            </S.ContentContainer>
 
-              )
-            })}
+            <S.SubTitle>Resumo</S.SubTitle>
+            <S.ContentContainer>
+               <S.Label>{candidate.resumo}</S.Label>
+            </S.ContentContainer>
+            <S.SubTitle>Formação</S.SubTitle>
+             <S.ContentContainer style={{height: '83px'}}>
+                  <S.Label><S.LabelStrong>Instituição:</S.LabelStrong>{candidate.formação[0].instituicao}</S.Label>
+                  <S.Label><S.LabelStrong>Curso:</S.LabelStrong> {candidate.formação[0].curso}</S.Label>
+             </S.ContentContainer>                
+           
+            <S.SubTitle>Experiência</S.SubTitle>
+            <S.ContentContainer style={{height: '120px', marginBottom: '18px'}}>
+                  <S.Label><S.LabelStrong>Empresa:</S.LabelStrong> {candidate.experiencia[0].empresa}</S.Label>
+                  <S.Label><S.LabelStrong>Cargo:</S.LabelStrong>{candidate.experiencia[0].cargo}</S.Label>
+                  <S.Label><S.LabelStrong>Tempo:</S.LabelStrong> {candidate.experiencia[0].tempo}</S.Label>            
+
+            </S.ContentContainer>
+            <S.SubTitle style={{margin: ' 0 0 17px 0'}}>Competências</S.SubTitle>
+            <S.ContentContainer style={{border:'none', margin: '0', height: '150px'}}>
+              {candidates.map(candidate => {
+                return (
+                  <S.Content>
+                    <S.LabelWithColor>{candidate.competencias}</S.LabelWithColor>
+                  </S.Content>
+                )
+              })}
+
+            </S.ContentContainer>
+            
             <Buton primary={true} name='Voltar' onClick={() => navigate('/list')} />
           </S.DivCard>
         )
--- front-end/src/pages/Profile/styled.js
+++ front-end/src/pages/Profile/styled.js
@@ -13,7 +13,7 @@ export const DivContainer = styled(Container) `
 ` 
 
 export const DivCard = styled(Card)`
-  width: 998px;  
+  width: 998px;    
   display: flex;
   flex-direction: column;
   align-items: start;
@@ -24,35 +24,60 @@ export const DivCard = styled(Card)`
   box-shadow: 0px 24px 32px rgba(0, 0, 0, 0.25);
 `
 
-export const CardBody = styled(Card.Body)`
+
+export const ContentContainer = styled(Container)`  
+  height: 170px;  
+  border-bottom: 2px solid #d9d9d9;
+  background-color: #fff;
   display: flex;
-  flex-direction: column;
+  flex-direction: column;  
+  align-items: start;
+  justify-content: flex-start; 
+  padding-left: 0;
+`
+
+export const Content = styled(Container)`  
+  display: flex;
   align-items: center;
-  justify-content: center;
-    
+  justify-content: space-between; 
+  padding-left: 0; 
+  
 `
 
 export const Title = styled(Card.Title)`
-  margin-bottom: 21px;
+  margin-bottom: 36px;
   font-size: 40px;
   line-height: 48.41px;
-  color: #979595;  
+  color: #767474;  
+`
+export const SubTitle = styled(Title)`
+  font-size: 24px;
+  line-height: 29px;
+  margin: 17px 0;
 `
 
 export const Label = styled(Form.Label)`
   font-size: 20px;
-  line-height: 24.2px;
+  line-height: 24px;
   font-weight: 400;
-  color: #979595;
+  color: #868686;
+   
 `
 
-export const Buton = styled(Button)`
-  width: 216.84px;
-  height: 68px;  
-  border-radius: 14px;
-  font-size: 24px;
-  color: #fff;
-  line-height: 29px;
-  border: none;
-  background-image: linear-gradient(91.76deg, #D42F43 0%, #D3823E 100%)
+export const LabelStrong = styled(Form.Label)`
+  font-weight: 500;
+  margin-right: 5px;
+  
 `
+
+export const LabelWithColor = styled(Label)`
+  font-family: 'Roboto', sans-serif;
+  font-size: 14px;
+  line-height: 16px;
+  color: #fff;  
+  background-color: #9BD26F;
+  padding: 8px 12px;
+  border-radius: 11px; 
+  margin-bottom: 170px; 
+`
+
