144045f068ef297ad976533709f9bed18153ebb7
Fabiana Barreto <39680930+fabifelicia@users.noreply.github.com>
2022-06-07 21:22:18 -0300

    add masks on input

--- front-end/src/masks.js
+++ front-end/src/masks.js
@@ -1,26 +1,26 @@
 export function cpf(e) {
-  e.currentTarget.maxLength = 9;
+  e.currentTarget.maxLength = 14;
   let value = e.currentTarget.value;
   value = value.replace(/\D/g, "");
-  value = value.replace(/^(\d{5})(\d)/, "$1-$2");
+  value = value.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
   e.currentTarget.value = value;
   return e;
 }
 
 export function data(e) {
-  e.currentTarget.maxLength = 9;
+  e.currentTarget.maxLength = 10;
   let value = e.currentTarget.value;
   value = value.replace(/\D/g, "");
-  value = value.replace(/^(\d{5})(\d)/, "$1-$2");
+  value = value.replace(/^(\d{2})(\d{2})(\d{4})/, "$1/$2/$3");
   e.currentTarget.value = value;
   return e;
 }
 
 export function phone(e) {
-  e.currentTarget.maxLength = 9;
+  e.currentTarget.maxLength = 14;
   let value = e.currentTarget.value;
   value = value.replace(/\D/g, "");
-  value = value.replace(/^(\d{5})(\d)/, "$1-$2");
+  value = value.replace(/^(\d{2})(\d{5})(\d{4})/, "($1)$2-$3");
   e.currentTarget.value = value;
   return e;
 }
